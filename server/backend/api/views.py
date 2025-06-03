0845da98243091fc53b762a111decd3bdefd7d58
ChandraShekhar <csyadav0513@gmail.com>
2025-06-03 18:16:27 +0530

    Implement viewsets for Cafe, Table, MenuItem, and Order models

--- server/backend/api/views.py
+++ server/backend/api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from .models import Cafe, Table, MenuItem, Order
+from .serializers import CafeSerializer, TableSerializer, MenuItemSerializer, OrderSerializer
 
 # Create your views here.
+
+from rest_framework import viewsets
+from .models import Cafe, Table, MenuItem, Order
+from .serializers import CafeSerializer, TableSerializer, MenuItemSerializer, OrderSerializer
+
+
+class CafeViewSet(viewsets.ModelViewSet):
+    queryset = Cafe.objects.all()
+    serializer_class = CafeSerializer
+
+
+class TableViewSet(viewsets.ModelViewSet):
+    queryset = Table.objects.all()
+    serializer_class = TableSerializer
+
+
+class MenuItemViewSet(viewsets.ModelViewSet):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
