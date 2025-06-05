62540a8838d30b9bee3a2e3b7ae67764061f860e
ChandraShekhar <csyadav0513@gmail.com>
2025-06-05 14:18:49 +0530

    Add Order Items URLs to API for CRUD operations

--- server/backend/api/urls.py
+++ server/backend/api/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import get_all_cafes , get_cafe_by_id, create_cafe, get_table_by_id, update_cafe, delete_cafe , get_all_tables , get_all_menu_items, get_menu_item_by_id, create_menu_item, update_menu_item, delete_menu_item , get_all_orders, get_order_by_id , create_order, update_order, delete_order 
+from .views import get_all_cafes, get_cafe_by_id, create_cafe, get_table_by_id, update_cafe, delete_cafe, get_all_tables, get_all_menu_items, get_menu_item_by_id, create_menu_item, update_menu_item, delete_menu_item, get_all_orders, get_order_by_id, create_order, update_order, delete_order, get_all_orders_item, get_order_item_by_id, create_order_item, update_order_item, delete_order_item
 
 
 urlpatterns = [
@@ -33,7 +33,13 @@ urlpatterns = [
     path("orders/update/<int:order_id>/", update_order, name="update_order"),  # Update order
     path("orders/delete/<int:order_id>/", delete_order, name="delete_order"),  # Delete order (soft delete)
 
-
+    #! Order Items URLs 🌨️
+    path("order-items/", get_all_orders_item, name="get_all_order_items"),  # Get all order items
+    path("order-items/<int:item_id>/", get_order_item_by_id,
+         name="get_order_item_by_id"),  # Get order item by ID
+    path("order-items/create/", create_order_item, name="create_order_item"),  # Create new order item
+    path("order-items/update/<int:item_id>/", update_order_item, name="update_order_item"),  # Update order item
+    path("order-items/delete/<int:item_id>/", delete_order_item, name="delete_order_item"),  # Delete order item (soft delete)
 
 ]
          
