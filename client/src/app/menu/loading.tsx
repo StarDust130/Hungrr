e54f6a48db727a050d83535b9eb42dd870813f6c
ChandraShekhar <csyadav0513@gmail.com>
2025-06-11 11:10:39 +0530

    Add Loading component for menu with centered loading message

---
+++ client/src/app/menu/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return (
+    <p className="h-screen w-full flex justify-center item-center ">
+      Loading Menu 😉...
+    </p>
+  );
+}
