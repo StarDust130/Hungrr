8b243a3f8654960d79031c17830a093ce30ad977
ChandraShekhar <csyadav0513@gmail.com>
2025-06-13 16:46:33 +0530

    Update Django settings to use PostgreSQL database and load environment variables from .env.prod

--- server/backend/backend/settings.py
+++ server/backend/backend/settings.py
@@ -10,7 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+# Explicitly load .env.prod
+load_dotenv(dotenv_path=BASE_DIR / ".env.prod")
+
+print("DB ENV 😇:", os.getenv("POSTGRES_DB"))
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -71,15 +83,21 @@ TEMPLATES = [
 WSGI_APPLICATION = "backend.wsgi.application"
 
 
-# Database
+# Database (postgresql)
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': os.getenv("POSTGRES_HOST", "localhost"),
+        'PORT': os.getenv("POSTGRES_PORT", "5432"),
     }
 }
+
 
 
 # Password validation
