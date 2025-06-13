c651bb594d5d7a360edd6ee5a066d1730176878c
ChandraShekhar <csyadav0513@gmail.com>
2025-06-13 17:52:47 +0530

    Update Cafe model to set default reviews count to 969

--- server/backend/api/models.py
+++ server/backend/api/models.py
@@ -15,7 +15,7 @@ class Cafe(models.Model):
     banner_url = models.URLField(max_length=200, blank=True)
     location = models.TextField()
     rating = models.DecimalField(max_digits=3, decimal_places=1, default=4.7)
-    reviews = models.PositiveIntegerField(default=0)
+    reviews = models.PositiveIntegerField(default=969)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
