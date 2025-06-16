1ec4ce5e1dc26f80aaefa9058f1b24a256547f18
ChandraShekhar <csyadav0513@gmail.com>
2025-06-16 15:39:32 +0530

    Refactor next.config.ts: update image configuration to use remotePatterns for improved flexibility

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "picsum.photos"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+      },
+    ],
   },
   typescript: {
     ignoreBuildErrors: true,
