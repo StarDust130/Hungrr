3e076847651622463dbc70eb6923415559493c5d
ChandraShekhar <csyadav0513@gmail.com>
2025-06-16 15:40:31 +0530

    Refactor MenuPage component: enhance error handling and update cafe data fetching logic

--- client/src/app/menu/[tableId]/page.tsx
+++ client/src/app/menu/[tableId]/page.tsx
@@ -2,6 +2,7 @@
 import CafeBanner from "@/components/menuComp/CafeBanner";
 import CartProvider from "@/components/menuComp/CartProvider";
 import MenuPageContent from "@/components/menuComp/MenuPageContent";
+import Image from "next/image";
 
 interface Props {
   params: {
@@ -12,27 +13,49 @@ interface Props {
 export default async function MenuPage({ params }: Props) {
   const slug = params.tableId;
 
-  console.log(`Fetching data for cafe with slug: ${slug}`);
-  
+  let cafeData = null;
 
-  const res = await fetch(`http://localhost:5000/api/cafe/${slug}`, {
-    next: { revalidate: 60 }, // Optional caching
-  });
+  try {
+    const res = await fetch(`http://localhost:5000/api/cafe/${slug}`, {
+      cache: "no-store",
+    });
 
-  console.log(`Response status: ${res.status}`);
-  console.log(`Response URL: ${res.url}`);
-  if (!res.ok) {
-    throw new Error("Failed to fetch cafe data");
+    if (res.status === 404) {
+      // Cafe not found
+      return (
+        <div className="p-10 flex flex-col justify-center items-center h-[80vh] text-center  font-bold ">
+          <Image
+            src={"/anime-girl-not-found.png"}
+            alt="Not Found"
+            width={200}
+            height={200}
+          />
+          <p className="text-base mt-3">
+            ❌ No cafe found with name{" "}
+            <span className="italic text-red-600">{slug}</span>{" "}
+          </p>
+        </div>
+      );
+    }
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch cafe data");
+    }
+
+    const { data } = await res.json();
+    cafeData = data;
+  } catch (error) {
+    console.error("Error loading cafe:", error);
+    return (
+      <div className="p-10 h-screen text-center text-xl font-bold text-red-600">
+        🚨 Error loading cafe data. Please try again later.
+      </div>
+    );
   }
-
-  const { data } = await res.json();
-
-  console.log("Fetched cafe data:", data);
-  
 
   return (
     <CartProvider>
-      <CafeBanner cafe={data} />
+      <CafeBanner cafe={cafeData} />
       <MenuPageContent />
     </CartProvider>
   );
