1a335aa115615a40496b71435c2e52e4d31ac566
ChandraShekhar <csyadav0513@gmail.com>
2025-06-16 14:20:40 +0530

    Refactor MenuPage component: implement async data fetching and enhance cafe data handling

--- client/src/app/menu/[tableId]/page.tsx
+++ client/src/app/menu/[tableId]/page.tsx
@@ -1,11 +1,39 @@
+// app/menu/[tableId]/page.tsx
+import CafeBanner from "@/components/menuComp/CafeBanner";
 import CartProvider from "@/components/menuComp/CartProvider";
 import MenuPageContent from "@/components/menuComp/MenuPageContent";
 
+interface Props {
+  params: {
+    tableId: string;
+  };
+}
 
-const MenuPage = () => (
-  <CartProvider>
-    <MenuPageContent />
-  </CartProvider>
-);
+export default async function MenuPage({ params }: Props) {
+  const slug = params.tableId;
 
-export default MenuPage;
+  console.log(`Fetching data for cafe with slug: ${slug}`);
+  
+
+  const res = await fetch(`http://localhost:5000/api/cafe/${slug}`, {
+    next: { revalidate: 60 }, // Optional caching
+  });
+
+  console.log(`Response status: ${res.status}`);
+  console.log(`Response URL: ${res.url}`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch cafe data");
+  }
+
+  const { data } = await res.json();
+
+  console.log("Fetched cafe data:", data);
+  
+
+  return (
+    <CartProvider>
+      <CafeBanner cafe={data} />
+      <MenuPageContent />
+    </CartProvider>
+  );
+}
