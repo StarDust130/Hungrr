6fbf7c1fd44c34c0278dd90880131832f9186440
ChandraShekhar <csyadav0513@gmail.com>
2025-06-17 14:55:43 +0530

    feat: add route for retrieving menu categories by slug

--- server/src/routes/cafeRoutes.ts
+++ server/src/routes/cafeRoutes.ts
@@ -1,10 +1,16 @@
 // src/routes/cafeRoutes.ts
 import { Router } from "express";
-import { getCafeInfoBySlug, getCafeMenu } from "../controllers/cafeController";
+import {
+  getCafeInfoBySlug,
+  getCafeMenu,
+  getCategory,
+} from "../controllers/cafeController";
 
 const router = Router();
 
 router.get("/cafe_banner/:slug", getCafeInfoBySlug);
+
 router.get("/menu/:slug/", getCafeMenu);
+router.get("/menu/category/:slug/", getCategory);
 
 export default router;
