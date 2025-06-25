f1e3c89045e1ce67ed4f5487e6b64af373330fe9
ChandraShekhar <csyadav0513@gmail.com>
2025-06-25 15:32:12 +0530

    refactor: move cafe logo URL assignment to generatePdf function for dynamic handling

--- client/src/components/billComp/GenerateReliablePdf.tsx
+++ client/src/components/billComp/GenerateReliablePdf.tsx
@@ -3,10 +3,6 @@ import { PDFBuilder } from "@/lib/PDFBuilder";
 import { BillData } from "@/types/menu";
 import QRCode from "qrcode";
 
-// --- CONFIGURATION ---
-const CAFE_LOGO_URL =
-  "https://images.unsplash.com/photo-1602934445884-da0fa1c9d3b3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGxvZ298ZW58MHx8MHx8fDA%3D";
-
 // Helper function to fetch an image and convert it to a Base64 Data URL
 const getImageAsDataUrl = (url: string): Promise<string> => {
   // This check ensures the function only runs in the browser
@@ -29,6 +25,7 @@ const getImageAsDataUrl = (url: string): Promise<string> => {
  * The main orchestrator function to generate the PDF.
  */
 const generatePdf = async ({ bill }: { bill: BillData }) => {
+  const CAFE_LOGO_URL = bill.logoUrl;
   try {
     // 1. Prepare async data (QR Code and Logo Data URL)
     const qrCodeDataUrl = await QRCode.toDataURL(window.location.href, {
