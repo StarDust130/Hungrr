fac8cd0c2f6cf1fa87fadbccc42c62a2c0e52d00
ChandraShekhar <csyadav0513@gmail.com>
2025-07-07 13:18:12 +0530

    refactor: remove mobile menu state management and related functionality from Navbar

--- client/src/components/elements/Navbar.tsx
+++ client/src/components/elements/Navbar.tsx
@@ -1,21 +1,10 @@
 "use client";
 import Image from "next/image";
 import { ModeToggle } from "../ui/ModeToggle";
-import { useState, useEffect } from "react";
-import { Menu,  X } from "lucide-react";
 import Link from "next/link";
 
 
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth >= 768) setIsMenuOpen(false);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
 
   return (
     <nav className=" top-0 left-0 right-0 z-50 backdrop-blur-md border-b border-border bg-background/70">
@@ -51,44 +40,9 @@ const Navbar = () => {
           {/* Right Controls */}
           <div className="flex items-center space-x-3">
             <ModeToggle />
-
-          
-
-          
-
-            {/* Hamburger menu for mobile */}
-            <button
-              className="md:hidden"
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-            >
-              {isMenuOpen ? (
-                <X className="w-6 h-6" />
-              ) : (
-                <Menu className="w-6 h-6" />
-              )}
-            </button>
           </div>
         </div>
 
-        {/* Mobile Menu */}
-        {isMenuOpen && (
-          <div className="md:hidden mt-4 border-t border-border pt-4 space-y-4 animate-in fade-in duration-200 text-sm">
-            {["features", "how-it-works", "pricing"].map((section) => (
-              <Link
-                key={section}
-                href={`#${section}`}
-                onClick={() => setIsMenuOpen(!isMenuOpen)}
-                className="block hover:text-primary transition-colors"
-              >
-                {section
-                  .split("-")
-                  .map((word) => word[0].toUpperCase() + word.slice(1))
-                  .join(" ")}
-              </Link>
-            ))}
-            
-          </div>
-        )}
       </div>
     </nav>
   );
