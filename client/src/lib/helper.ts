31d19d23ec77d33a48b3c7cd6607b3ade1e1a8ab
ChandraShekhar <csyadav0513@gmail.com>
2025-07-13 09:06:46 +0530

    now log in prod

--- client/src/lib/helper.ts
+++ client/src/lib/helper.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // utils/log.ts
 export const log = (...args: any[]) => {
-  if (process.env.NODE_ENV === "development") {
+  // if (process.env.NODE_ENV === "development") {
     console.log(...args);
-  }
+  // }
 };
 
 
